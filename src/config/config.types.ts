e8b617ec14c2dfd1e4f03726d4e1abd7e731ec9c
awpt1mus <kamehamehabeech@gmail.com>
2024-04-23 18:57:19 +0530

    feat(config): add types, zod schema for validating config and custom loader

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,6 +1,23 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const EnvironmentVariableSchema = z.object({
+export const EnvironmentVariablesSchema = z.object({
   port: z.coerce.number().safe().min(0).max(65535),
   secret: z.string().min(1),
+  db_user: z.string().min(1),
+  db_pass: z.string().min(1),
+  db_name: z.string().min(1),
+  db_port: z.coerce.number().safe().min(0).max(65535),
+  db_host: z.string().min(1),
 });
+
+export interface AppConfig {
+  port: number;
+  jwtSecret: string;
+  database: {
+    port: number;
+    name: string;
+    host: string;
+    user: string;
+    password: string;
+  };
+}
