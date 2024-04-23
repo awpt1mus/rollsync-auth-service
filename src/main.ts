98fcb4ed8885bb902b5f6e773d64e57164c05a6a
awpt1mus <kamehamehabeech@gmail.com>
2024-04-23 18:58:50 +0530

    refactor: use port read from config service when bootstraping server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { RootModule } from './root/root.module';
+import { NestFactory } from "@nestjs/core";
+import { RootModule } from "./root/root.module";
+import { ConfigService } from "@nestjs/config";
+import type { AppConfig } from "./config/config.types";
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService<AppConfig>);
+  const port = configService.get<number>("port");
+  await app.listen(port);
 }
 bootstrap();
