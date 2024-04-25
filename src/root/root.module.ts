f58a5c35504956069ea91c9538c8858d5217d753
awpt1mus <kamehamehabeech@gmail.com>
2024-04-25 19:20:39 +0530

    mount auth module in root module

--- src/root/root.module.ts
+++ src/root/root.module.ts
@@ -1,19 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import loadConfig from "../config/config.loader";
-import { connectionProvider } from "../database/connection.provider";
 import { RootController } from "./root.controller";
-
+import { AuthModule } from "src/auth/auth.module";
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			ignoreEnvFile: process.env.NODE_ENV === "production",
-			envFilePath: ".env",
-			isGlobal: true,
-			load: [loadConfig],
-		}),
-	],
-	providers: [connectionProvider],
-	controllers: [RootController],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+      envFilePath: ".env",
+      isGlobal: true,
+      load: [loadConfig],
+    }),
+    AuthModule,
+  ],
+  controllers: [RootController],
 })
 export class RootModule {}
