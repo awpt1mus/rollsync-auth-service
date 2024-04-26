9966274f83a9927cde341aeaa6bc2461c651c6c9
awpt1mus <kamehamehabeech@gmail.com>
2024-04-26 16:36:30 +0530

    add auth guard to validate jwt token and extract user

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+} from "@nestjs/common";
+import { UserRepository } from "src/database/repositories/user.repository";
+import { TokenService } from "./jwt.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+	constructor(
+		private readonly tokenService: TokenService,
+		private readonly userRepository: UserRepository,
+	) {}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+
+		const [type, token] = request.headers.authorization?.split(" ") ?? [];
+
+		if (!(type === "Bearer" && token)) {
+			throw new UnauthorizedException();
+		}
+
+		const { payload, success } = await this.tokenService.validateToken(token);
+
+		if (!success) {
+			throw new UnauthorizedException();
+		}
+
+		const { id } = payload;
+
+		request.user = await this.userRepository.findById(id);
+
+		return true;
+	}
+}
